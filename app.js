1f25963519e3e4f4197f17c01d6e33c32374c1a2
ericpcho <ericpcho@gmail.com>
2017-08-28 09:34:51 -0700

    Created javascript file with STORE data and master function

---
+++ app.js
@@ -0,0 +1,25 @@
+const STORE = {
+  page: ['intro', 'questions', 'results', 'finalpage'],
+  questions: [
+    {
+      id: 1,
+      questionText: 'This is Question 1 for our Quiz app',
+      answers: [
+        'This is Answer 1',
+        'This is Answer 2',
+        'This is Answer 3',
+        "This is Answer 4"
+      ],
+      correctAnswer: answers[0]
+    }
+  ]
+}
+
+// Object 1 thing
+// Arrary = many things
+
+function handleQuizApp(); {
+
+}
+
+$(handleQuizApp);
