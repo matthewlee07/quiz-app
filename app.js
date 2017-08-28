17acc17ab0eab7eb7df5b3d9699cf2a20fa3d3b2
ericpcho <ericpcho@gmail.com>
2017-08-28 10:15:22 -0700

    added section header for html first page

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const STORE = {
-  page: ['intro', 'questions', 'results', 'finalpage'],
-  questions: [
-    {
-      id: 1,
-      questionText: 'This is Question 1 for our Quiz app',
-      answers: [
-        'This is Answer 1',
-        'This is Answer 2',
-        'This is Answer 3',
-        "This is Answer 4"
-      ],
-      correctAnswer: answers[0]
-    }
-  ]
-}
+const STORE =
+  {
+    page: ['intro', 'questions', 'results', 'finalpage'],
+    questions:
+    [
+      {
+        id: 1,
+        questionText: 'This is Question 1 for our Quiz app',
+        answers: [
+          'This is Answer 1',
+          'This is Answer 2',
+          'This is Answer 3',
+          "This is Answer 4"
+        ],
+        correctAnswer: answers[0]
+      }
+    ]
+  }
 
-// Object 1 thing
-// Arrary = many things
+// a function to render/handle and display each page separately page.[1], page.2[],
 
+
+// Master function to handle all our other functions. Runs everything. 
 function handleQuizApp(); {
 
 }
