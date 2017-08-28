7334f083dd3f63d530ecb7f5c53c3721891072f0
ericpcho <ericpcho@gmail.com>
2017-08-28 11:27:27 -0700

    added some starter functions and event listener for submit startquiz

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
 const STORE =
   {
-    page: ['intro', 'questions', 'results', 'finalpage'],
+    page: ['.js-quiz-intro', '.js-quiz-selection', '.js-feedback-page', '.js-score-result'],
     questions:
     [
       {
         id: 1,
         questionText: 'This is Question 1 for our Quiz app',
-        answers: [
-          'This is Answer 1',
-          'This is Answer 2',
-          'This is Answer 3',
-          "This is Answer 4"
-        ],
-        correctAnswer: answers[0]
+        answers: ['This is Answer 1', 'This is Answer 2', 'This is Answer 3', "This is Answer 4"],
+        correctAnswer: 0
       }
     ]
   }
 
-// a function to render/handle and display each page separately page.[1], page.2[],
 
+// a function to render/handle and display each page separately page.[1], page.2[], ...etc
+function handleIntroPage(){
+  console.log('handleDisplayPage ran');
+  
+}
+
+function submitIntroPage(){
+  $('.js-start-game').submit(function (event) {
+    event.preventDefault();
+  }
+  )}
+
+function handleQuestionsPage(){
+  console.log('handleQuestionsPage ran');
+  
+}
+
+function handleResultsPage(){
+  console.log('handleResultsPage ran');
+  
+}
+
+function handleFinalPage(){
+  console.log('handleFinalPage ran');
+  
+}
 
 // Master function to handle all our other functions. Runs everything. 
-function handleQuizApp(); {
+function handleQuizApp() {
+  handleIntroPage();
 
 }
 
 $(handleQuizApp);
+
+
+// const q = questions[0];
+// q.answers
+// q.answer[q.correctAnswer]
