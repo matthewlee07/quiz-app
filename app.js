2a7787f9d6d83a22860ff616ddcd8b7600803a0d
ericpcho <ericpcho@gmail.com>
2017-08-29 07:44:44 -0700

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const STORE =
   {
-    pageState: ['.js-quiz-intro', '.js-quiz-selection', '.js-feedback-page', '.js-score-result'],
+    correct: "Correct!",
+    pageState: "page1", 
     totalQuestions: 0,
     correctQuestions: 0,
     questions:
@@ -17,112 +18,148 @@ const STORE =
         answers: ['This is Answer 1', 'This is Answer 2', 'This is Answer 3', "This is Answer 4"],
         correctAnswer: 3
       },
-      // {
-      //   id: 1,
-      //   questionText: 'This is Question 1 for our Quiz app',
-      //   answers: ['This is Answer 1', 'This is Answer 2', 'This is Answer 3', "This is Answer 4"],
-      //   correctAnswer: 0
-      // },
-      // {
-      //   id: 1,
-      //   questionText: 'This is Question 1 for our Quiz app',
-      //   answers: ['This is Answer 1', 'This is Answer 2', 'This is Answer 3', "This is Answer 4"],
-      //   correctAnswer: 0
-      // },
-      // {
-      //   id: 1,
-      //   questionText: 'This is Question 1 for our Quiz app',
-      //   answers: ['This is Answer 1', 'This is Answer 2', 'This is Answer 3', "This is Answer 4"],
-      //   correctAnswer: 0
-      // },
-      // {
-      //   id: 1,
-      //   questionText: 'This is Question 1 for our Quiz app',
-      //   answers: ['This is Answer 1', 'This is Answer 2', 'This is Answer 3', "This is Answer 4"],
-      //   correctAnswer: 0
-      // },
+      {
+        id: 3,
+        questionText: 'This is Question 3 for our Quiz app',
+        answers: ['This is Answer 1', 'This is Answer 2', 'This is Answer 3', "This is Answer 4"],
+        correctAnswer: 2
+      },
+      {
+        id: 4,
+        questionText: 'This is Question 4 for our Quiz app',
+        answers: ['This is Answer 1', 'This is Answer 2', 'This is Answer 3', "This is Answer 4"],
+        correctAnswer: 1
+      },
+      {
+        id: 5,
+        questionText: 'This is Question 5 for our Quiz app',
+        answers: ['This is Answer 1', 'This is Answer 2', 'This is Answer 3', "This is Answer 4"],
+        correctAnswer: 3
+      },
+      {
+        id: 6,
+        questionText: 'This is Question 5 for our Quiz app',
+        answers: ['This is Answer 1', 'This is Answer 2', 'This is Answer 3', "This is Answer 4"],
+        correctAnswer: 0
+      },
     ]
   }
 
 
 function renderQuizApp() {
-  $(".container").html();
+$('section').addClass("hidden");
+
+if (STORE.pageState === "page1") {
+  $('.js-quiz-intro').removeClass("hidden");
+}
+if (STORE.pageState === "page2") {
+  $('.js-quiz-selection').removeClass("hidden");}
+  i = STORE.totalQuestions
+  $(".js-questionNumber-question").html(`<h2>Question ${STORE.questions[i].id}</h2>
+    <p>${STORE.questions[i].questionText}</p>`)
+
+  $(".question-container").html(`
+  <input type="radio" checked="checked" class="js-optionA" name="option" value="0"></input><label class="js-answersA">${STORE.questions[i].answers[0]}</label>
+  <input type="radio"  class="js-optionB" name="option" value="1"></input><label class="js-answersB">${STORE.questions[i].answers[1]}</label>
+  <input type="radio"  class="js-optionC" name="option" value="2"></input><label class="js-answersC">${STORE.questions[i].answers[2]}</label>
+  <input type="radio"  class="js-optionD" name="option" value="3"></input><label class="js-answersD">${STORE.questions[i].answers[3]}</label>
+  `)
+
+if (STORE.pageState === "page3") {
+    $('.js-quiz-feedback').removeClass("hidden");}
+    $('.js-feedback-container').html(`
+    <h3 class="js-feedback-header">${STORE.correct}</h3>
+    <p class="js-feedback-text">Correct Answer: ${STORE.questions[i].answers[STORE.questions[i].correctAnswer]}</p>
+    <p class="js-feedback-answer">Score: ${STORE.correctQuestions}/${STORE.totalQuestions}</p>
+    <button type="submit" class="js-next-question">Next Question</button>`)
+
+if (STORE.pageState === "page4") {
+    $('.js-quiz-result').removeClass("hidden");
+    $('.js-score-container').html(`<p class="js-score-text">You got ${STORE.correctQuestions} of ${STORE.totalQuestions} questions!</p> <button type="submit">Play Again</button>`)
+  }
+
+
+
+
+
 }
 
-function generateStrings() {
-  
+function startQuiz(){
+ STORE.pageState = "page2"
 }
 
-function generateIntroPage() {
-  console.log('handleDisplayPage ran');
-  return `
-  <section class="js-quiz-intro quiz-intro hidden">
-    <h1>Who Wants to Be a Millionaire?</h1>
-    <div>
-      <img src="" alt="">
-          </div>
-      <button type="submit" class="js-start-game">Start Game</button>
-  </section>`
-    // submitIntroPage();
-  
-  renderQuizApp();
+
+
+function submitIntroPage() {
+      $('.js-quiz-intro').on("click", ".js-start-game", function (event) {
+        event.preventDefault();
+        startQuiz();
+        renderQuizApp();
+      }
+      )
+    }
+
+function getResult(){
+  STORE.pageState = "page3"
 }
 
-// function submitIntroPage() {
-//       $('.js-quiz-intro').on("click", ".js-start-game", function (event) {
-//         event.preventDefault();
-//         console.log('submit works')
-//       }
-//         //
-//       )
-//     }
+function logResult(){
+  STORE.totalQuestions++;
+  if (STORE.questions[i].correctAnswer === parseInt(finalAnswer)){
+  STORE.correctQuestions++; 
+  STORE.correct = "Correct!"
+  }
+  else {
+  STORE.correct = "Wrong!"
+  }
+}
 
-//     function handleQuestionsPage() {
-//       //insert question & answers
-//       $('.js-quiz-selection').on("click", ".js-final-answer", function (event) {
-//         event.preventDefault();
-//         console.log('handleQuestionsPage ran');
-//         finalAnswerSubmit();
-//       })
-//       //STORE.questions.questionText/answers
-//     }
+function goToFinal (){
+  if (STORE.totalQuestions === 5) {
+    STORE.pageState = "page4"
+  }
+}
 
-//     function finalAnswerSubmit() {
-//       console.log('handleRadioSubmit ran');
+function handleQuestionsPage() {
+      //insert question & answers
+      $('.js-quiz-selection').on("click", ".js-final-answer", function (event) {
+        event.preventDefault();
+        finalAnswer = $('input[type="radio"]:checked').val()
+        console.log('handleQuestionsPage ran');
+        getResult();
+        logResult();
+        goToFinal();
+        renderQuizApp();
+      })
+    }
 
-//     }
+function handleNextQuestion(){
+  if (STORE.totalQuestions === 5){
+  STORE.pageState = "page4"
+  }
+  else {
+  STORE.pageState = "page2"
+  }
+}
 
-// function handleResultsPage() {
-//       $('.js-quiz-feedback').on("click", ".js-next-question", function (event) {
-//         event.preventDefault();
-//         console.log('handleResultsPage ran');
-//         //update X/Y correct answers
-//         //see if there's another question or end of questions
-//         //if () go to next question
-//         // $('.js-quiz-selection').removeClass("hidden");
-//         //else go to final page
-//       })
-//     }
+function handleResultsPage() {
+      $('.js-quiz-feedback').on("click", ".js-next-question", function (event) {
+        event.preventDefault();
+        console.log('handleResultsPage ran');
+        handleNextQuestion();
+        renderQuizApp();
+      })
+    }
 
-//     function handleFinalPage() {
-
-//       console.log('handleFinalPage ran');
-//     //update X/Y correct answers
-//     //reset button
-//     }
-
-// function handleResetButton() {
-//       //goes back to start page
-//       console.log('handleResetButton ran');
-//     }
 
 // Master function to handle all our other functions. Runs everything.
 function handleQuizApp() {
       renderQuizApp();
-      generateIntroPage();
+      submitIntroPage();
+  // generateIntroPage();
   // handleIntroPage();
-  // handleQuestionsPage();
-  // handleResultsPage();
+  handleQuestionsPage();
+  handleResultsPage();
   // handleFinalPage();
   // handleResetButton();
 }
